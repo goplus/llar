412cece79cb135d270e34ea86958a5d3efc09343
xgopilot <noreply@goplus.org>
2026-01-30 10:14:03 +0000

    fix: update copyright to The XGo Authors (xgo.dev)
    Changed copyright headers in all 16 source files from
    "The llar Authors" to "The XGo Authors (xgo.dev)" format.
    
    Generated with [codeagent](https://github.com/qbox/codeagent)
    Co-authored-by: luoliwoshang <51194195+luoliwoshang@users.noreply.github.com>

--- internal/ixgo/pkgs.go
+++ internal/ixgo/pkgs.go
@@ -1,4 +1,4 @@
-// Copyright 2024 The llar Authors. All rights reserved.
+// Copyright (c) 2026 The XGo Authors (xgo.dev). All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
